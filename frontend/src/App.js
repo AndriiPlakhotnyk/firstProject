06da51028d809b543a98a8f1d1495b194b631629
Andrii <anderplaxot@gmail.com>
2022-11-28 17:55:49 +0200

    table crud

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const initialVAlues = {
+const initialValues = {
   userName: '',
   userSurname: '',
   userEmail: '',
@@ -9,16 +9,53 @@ const initialVAlues = {
 }
 
 function App() {
-  const [userData, setUserData] = useState(initialVAlues);
-  const [users, setUsers] = useState([])
+  const [userData, setUserData] = useState(initialValues);
+  const [users, setUsers] = useState([]);
+  const [editableUserData, setEditableUserData] = useState({
+    isEdit: false,
+    userIndex: null
+  })
+
+  const handleRemoveClick = (index) => {
+    setUsers(users.filter((user, userIndex) => userIndex !== index));
+  }
+
+  const isFilledFields = userData.userName && userData.userSurname && userData.userEmail && userData.userAge;
 
   const handleSubmitUser = (e) => {
     e.preventDefault();
 
-    setUsers((prevState) => [...prevState,userData]);
+    if (isFilledFields) {
+      if(editableUserData.isEdit) {
+        const editedData = users;
+        editedData.splice(editableUserData.userIndex, 1, userData)
 
-  setUserData(initialVAlues);
+        setUsers(editedData);
+
+        setEditableUserData({
+          isEdit: false,
+          userIndex: null
+        })
+      } else {
+        setUsers((prevState) => [...prevState,userData]);
+      }
+
+      
+
+      setUserData(initialValues);
+    }
   }
+
+  const handleCleanClick = () => setUserData(initialValues);
+  
+  const handleEditClick = (data, index) => {
+    setUserData(data);
+    setEditableUserData({
+      isEdit: true,
+      userIndex: index
+    })
+  }
+
 
   console.log('users: ', users)
 
@@ -27,7 +64,7 @@ function App() {
       <div className="wrapper-content">
         <div className="table-data">
           <table>
-            <th>id</th>
+            <th>№</th>
             <th>User Name</th>
             <th>User Surname</th>
             <th>email</th>
@@ -35,13 +72,28 @@ function App() {
             <th>Actions</th>
 
             <tbody>
+              {users.map((user, index) => (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{user.userName}</td>
+                  <td>{user.userSurname}</td>
+                  <td>{user.userEmail}</td>
+                  <td>{user.userAge}</td>
+                  <td>
+                    <div>
+                      <button className="edit-action" onClick={() => handleEditClick(user, index)}>edit</button>
+                      <button classNAme="remove-action" onClick={() => handleRemoveClick(index)}>remove</button>
+                    </div>
+                  </td>
 
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
 
         <div>
-          <form onSubmit={handleSubmitUser}>
+          <form onSubmit={handleSubmitUser} onReset={handleCleanClick}>
             <input placeholder='Write your name' onChange={(e) => setUserData((prevState) => ({
               ...prevState,
               userName: e.target.value
@@ -69,7 +121,7 @@ function App() {
 
             <div className="buttons-wrapper">
               <button type = "reset">Clean</button>
-              <button type = "submit">Add</button>
+              <button disabled={!isFilledFields} type = "submit">{editableUserData.isEdit ? 'Edit' : 'Add'}</button>
             </div>
           </form>
         </div>
